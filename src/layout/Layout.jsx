8fe0a5fbd65355c5e4d781d7d2d2848981893be1
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-01-07 18:44:41 +0330

    feat: layout folder created

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,8 +1,12 @@
-const Layout = ({children}) => {
-    return <div>
-    <h1>this is layout component</h1>
-    {children}
-    </div>;
-}
- 
+import Navigation from "../components/Navigation/Navigation";
+import styles from "./layout.module.css"
+const Layout = ({ children }) => {
+  return (
+    <div className={styles.siteLayout}>
+      <Navigation />
+      {children}
+    </div>
+  );
+};
+
 export default Layout;

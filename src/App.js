6e7c4d4d9d86483c5346e6ac0e532e2bb7de07e6
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-01-08 16:38:12 +0330

    fix: app js updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import CartPage from "./pages/CartPage/CartPage";
 import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="cart" element={<CartPage />} />
+
+        {/* <HomePage /> */}
+      </Routes>
     </div>
   );
 }

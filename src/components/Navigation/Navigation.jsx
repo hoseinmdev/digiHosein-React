c7428857cfff580f9e05ce5d92da5400987deac1
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-01-08 16:38:01 +0330

    feat: navigation component added

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,18 +1,45 @@
 import styles from "./navigation.module.css";
-import { FaShoppingCart, FaHome } from "react-icons/fa";
+import { FaShoppingCart, FaHome, FaSearch } from "react-icons/fa";
 import { FcShop } from "react-icons/fc";
+import { Link, NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <div className={styles.siteNvigation}>
-      <div className={styles.iconBlock}>
-        سبد خرید
-        <FaShoppingCart />
+      <div>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? `${styles.isActive}` : `${styles.iconBlock}`
+          }
+          to="/"
+        >
+          <div>
+            <FaHome />
+            خانه
+          </div>
+          <span></span>
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? `${styles.isActive}` : `${styles.iconBlock}`
+          }
+          to="/cart"
+        >
+          <div>
+            <FaShoppingCart />
+            سبد خرید
+          </div>
+          <span></span>
+        </NavLink>
       </div>
-      فروشگاه عمو حسین
-      <div className={styles.iconBlock}>
-        خانه
-        <FaHome />
+      <div>
+        <label>
+          جستجو کنید :
+          <div>
+            <FaSearch />
+            <input placeholder="جستجو برای ... " />
+          </div>
+        </label>
       </div>
     </div>
   );

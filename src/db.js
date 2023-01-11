a83e3994f8228836ab0e7efbbbe6b4a41dface6d
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-01-11 14:21:20 +0330

    feat: photos of products added to db

---
+++ src/db.js
@@ -0,0 +1,240 @@
+import iphone13 from "./Accets/images/iphone-13.png"
+import galaxyS21 from "./Accets/images/galaxy-s21.png"
+import iphone14 from "./Accets/images/iphone-14.png"
+import xiaomiPocoM4 from "./Accets/images/poco-m4.webp"
+import galaxyA51 from "./Accets/images/galaxy-A51.png"
+import partyBox from "./Accets/images/Mingo-Party-Box.png"
+import airpodsPro from "./Accets/images/airpod-pros-removebg-preview.png"
+import ps5 from "./Accets/images/Playstation-5.webp"
+import xboxSeriesS from "./Accets/images/Xbox-Series-S.webp"
+import ps4 from "./Accets/images/Playstation-4.png"
+import xboxOneS from "./Accets/images/Xbox-one-s.png"
+import asusRogStrix from "./Accets/images/asus-rog-strix.png"
+import macbookAir2020 from "./Accets/images/macbook-air-2020.png"
+import asusTufDashF15 from "./Accets/images/asus-tuf-dash-F15.png"
+import asusGaming from "./Accets/images/Asus-TUF-Gaming-F15-FX506-idigi-768x768-1.png"
+import zenBook from "./Accets/images/zenbook.png"
+import thinkBook from "./Accets/images/lenovo-thinkbook.webp"
+export const productsData = [
+  {
+    id: 1,
+    title: "موبایل اپل مدل iPhone 13",
+    price: 52439000,
+    imageURL: iphone13,
+    Specifications: {
+      speaker: "اسپیکر / استریو ( دوگانه )",
+      ram: "رم / 4 گیگابایت رم",
+      camera: "دوربین اصلی / 12 مگاپیکسل",
+      storage: "حافظه داخلی / 256 گیگابایت",
+      battery: "باتری / 3240 میلی آمپر ساعت",
+    },
+  },
+  {
+    id: 3,
+    title: " موبایل سامسونگ Galaxy s21",
+    price: 37199000,
+    imageURL: galaxyS21,
+    Specifications: {
+      speaker: "اسپیکر / استریو ( دوگانه )",
+      ram: "رم / 8 گیگابایت رم",
+      camera: "دوربین اصلی / 12 مگاپیکسل",
+      storage: "حافظه داخلی / 256 گیگابایت",
+      battery: "باتری / 4000 میلی آمپر ساعت",
+    },
+  },
+  {
+    id: 5,
+    title: "موبایل اپل مدل iPhone 14",
+    price: 48000000,
+    imageURL: iphone14,
+    Specifications: {
+      speaker: "اسپیکر / استریو ( دوگانه )",
+      ram: "رم / 6 گیگابایت رم",
+      camera: "دوربین اصلی / 12 مگاپیکسل",
+      storage: "حافظه داخلی / 512 گیگابایت",
+      battery: "باتری / 3279 میلی آمپر ساعت",
+    },
+  },
+  {
+    id: 6,
+    title: "موبایل شیائومی مدل Poco M4",
+    price: 6799000,
+    imageURL: xiaomiPocoM4,
+    Specifications: {
+      speaker: "اسپیکر / استریو ( دوگانه )",
+      ram: "رم / 8 گیگابایت رم",
+      camera: "دوربین اصلی / 50 مگاپیکسل",
+      storage: "حافظه داخلی / 128 گیگابایت",
+      battery: "باتری / 5000 میلی آمپر ساعت",
+    },
+  },
+  {
+    id: 8,
+    title: "موبایل سامسونگ Galaxy A51",
+    price: 8200000,
+    imageURL: galaxyA51,
+    Specifications: {
+      speaker: "اسپیکر / مونو",
+      ram: "رم / 6 گیگابایت رم",
+      camera: "دوربین اصلی / 48 مگاپیکسل",
+      storage: "حافظه داخلی / 128 گیگابایت",
+      battery: "باتری / 4000 میلی آمپر ساعت",
+    },
+  },
+  {
+    id: 2,
+    title: "پارتی باکس",
+    price: 5200000,
+    imageURL: partyBox,
+    Specifications: {
+      speaker: "ابعاد اسپیکر / 290x260x670",
+      ram: "میکروفون / دارد",
+      camera: "رقص نور / دارد",
+      storage: "نوع حافظه / حافظه داخلی فلش",
+      battery: "کارکرد باتری / 6 ساعت",
+    },
+  },
+  {
+    id: 4,
+    title: "هدفون بی سیم AirPods Pro",
+    price: 5795000,
+    imageURL: airpodsPro,
+    Specifications: {
+      speaker: "نوع اتصال / بی سیم",
+      ram: "رنگ / سفید",
+      camera: "میکروفون / دارد ",
+      storage: "کنترل کننده صدا / دارد",
+      battery: "طریقه شارژ / از طریق محفظه",
+    },
+  },
+  {
+    id: 9,
+    title: "کنسول بازی Play Station 5",
+    price: 22399000,
+    imageURL: ps5,
+    Specifications: {
+      cpu: "پردازنده / 8 هسته ای ذن",
+      ram: "حافظه رم / 16 گیگابایت رم",
+      camera: "خروجی تصویر / 4K , 8K",
+      storage: "حافظه داخلی / 825 گیگابایت",
+      Weight: "وزن / 4.5 کیلوگرم",
+    },
+  },
+  {
+    id: 10,
+    title: "کنسول بازی Xbox Seires s",
+    price: 10170000,
+    imageURL: xboxSeriesS,
+    Specifications: {
+      cpu: "پردازنده / شخصی سازی شده Zen2",
+      ram: "حافظه رم / 10 گیگابایت رم",
+      camera: "خروجی تصویر / QHD",
+      storage: "حافظه داخلی / 500 گیگابایت SSD",
+      Weight: "وزن / 1.93 کیلوگرم",
+    },
+  },
+  {
+    id: 11,
+    title: "کنسول بازی Play Station 4",
+    price: 15750000,
+    imageURL: ps4,
+    Specifications: {
+      cpu: "پردازنده / 8 هسته ای AMD",
+      ram: "حافظه رم / 8 گیگابایت رم",
+      camera: "خروجی تصویر / 1080p",
+      storage: "حافظه داخلی / 500 گیگابایت",
+      Weight: "وزن / 2.08 کیلوگرم",
+    },
+  },
+  {
+    id: 12,
+    title: "کنسول بازی Xbox وان اس",
+    price: 8300000,
+    imageURL: xboxOneS,
+    Specifications: {
+      cpu: "پردازنده / 8 هسته ای AMD",
+      ram: "حافظه رم / 8 گیگابایت رم",
+      camera: "خروجی تصویر / 4K",
+      storage: "حافظه داخلی / 1 ترابایت",
+      Weight: "وزن / 2.9 کیلوگرم",
+    },
+  },
+  {
+    id: 13,
+    title: "لپتاپ Asus ROG Strix",
+    price: 39648000,
+    imageURL: asusRogStrix,
+    Specifications: {
+      cpu: "پردازنده / اینتل core i7",
+      ram: "حافظه رم / 32 گیگابایت رم",
+      gpu: "گرافیک / انویدیا GTX 1650Ti",
+      storage: "حافظه داخلی / 1 ترابایت",
+      Weight: "وزن / 2.39 کیلوگرم",
+    },
+  },
+  {
+    id: 14,
+    title: "لپتاپ Macbook air 2020",
+    price: 32599000,
+    imageURL: macbookAir2020,
+    Specifications: {
+      cpu: "پردازنده / 8 هسته ای Apple M1",
+      ram: "حافظه رم / 8 گیگابایت رم",
+      gpu: "گرافیک / 7 هسته ای",
+      storage: "حافظه داخلی / 256 گیگابایت",
+      Weight: "وزن / 1.29 کیلوگرم",
+    },
+  },
+  {
+    id: 15,
+    title: "لپتاپ Asus TUF dash F15",
+    price: 42899000,
+    imageURL: asusTufDashF15,
+    Specifications: {
+      cpu: "پردازنده / اینتل core i7",
+      ram: "حافظه رم / 16 گیگابایت رم",
+      gpu: "گرافیک / انویدیا RTX 3060",
+      storage: "حافظه داخلی / 1 ترابایت",
+      Weight: "وزن / 2 کیلوگرم",
+    },
+  },
+  {
+    id: 16,
+    title: "لپتاپ Asus TUF Gaming F15",
+    price: 23899000,
+    imageURL: asusGaming,
+    Specifications: {
+      cpu: "پردازنده / اینتل core i5",
+      ram: "حافظه رم / 8 گیگابایت رم",
+      gpu: "گرافیک / انویدیا GTX 1650 Ti",
+      storage: "حافظه داخلی / 1256 گیگابایت",
+      Weight: "وزن / 2.3 کیلوگرم",
+    },
+  },
+  {
+    id: 17,
+    title: "لپتاپ Zenbook duo",
+    price: 49199000,
+    imageURL: zenBook,
+    Specifications: {
+      cpu: "پردازنده / اینتل core i7",
+      ram: "حافظه رم / 16 گیگابایت رم",
+      gpu: "گرافیک / انویدیا GeForce MX450",
+      storage: "حافظه داخلی / 1 ترابایت",
+      Weight: "وزن / 1.5 کیلوگرم",
+    },
+  },
+  {
+    id: 18,
+    title: "لپتاپ Lenovo Thinkbook",
+    price: 14800000,
+    imageURL: thinkBook,
+    Specifications: {
+      cpu: "پردازنده / اینتل core i5",
+      ram: "حافظه رم / 8 گیگابایت رم",
+      gpu: "گرافیک / 2 گیگابایت از نوع GDDR5",
+      storage: "حافظه داخلی / 1 ترابایت",
+      Weight: "وزن / 1.7 کیلوگرم",
+    },
+  },
+];

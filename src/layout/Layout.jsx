6e3f4f12bdc98d3b99a88ca62e9e8dc54445b15c
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-01-11 14:23:02 +0330

    feat: fade loading added

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,18 @@
+import { useEffect, useState } from "react";
 import Navigation from "../components/Navigation/Navigation";
-import styles from "./layout.module.css"
+import styles from "./layout.module.css";
 const Layout = ({ children }) => {
+  const [fade, setFade] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setFade(1);
+    }, 100);
+  }, []);
+
   return (
     <div className={styles.siteLayout}>
       <Navigation />
-      {children}
+      <div style={{ opacity: fade }}>{children}</div>
     </div>
   );
 };

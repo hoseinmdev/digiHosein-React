025f66f7f1125886a077c610ac7b0a121d2d0766
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-01-17 13:39:51 +0330

    fix: bug fixed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import CartProvider from "./context/CartProvider";
 import CartPage from "./pages/CartPage/CartPage";
 import HomePage from "./pages/HomePage/HomePage";
-
+import "react-toastify/dist/ReactToastify.css";
+import Toast from "./components/common/Toast/Toast";
+import SingleProductPage from "./pages/SingleProductPage/SingleProductPage";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="cart" element={<CartPage />} />
-      </Routes>
+      <CartProvider>
+        <Toast />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="cart" element={<CartPage />} />
+          <Route path="product" element={<SingleProductPage/>}/>
+        </Routes>
+      </CartProvider>
     </div>
   );
 }

5df6faf565618be27cc17dcf4cf161686bed9b57
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-01-24 21:43:40 +0330

    feat: buyed product component updated

--- src/components/CartProduct/CartProduct.jsx
+++ src/components/CartProduct/CartProduct.jsx
@@ -1,12 +1,28 @@
 import { useCart } from "../../context/CartProvider";
 import styles from "./cartProduct.module.css";
-import { FaTrash } from "react-icons/fa";
-import { AiOutlineMinus } from "react-icons/ai";
+import { FaCheckCircle, FaRibbon, FaSketch, FaTrash } from "react-icons/fa";
+import { AiOutlineClose, AiOutlineMinus } from "react-icons/ai";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CartProduct = ({ title, price, imageURL, quantity, id }) => {
   const { dispatch } = useCart();
   const [fade, setFade] = useState(1);
+  const history = useNavigate();
+  const renderProductPage = () => {
+    history(
+      { pathname: `/product/${id}` },
+      {
+        state: {
+          title,
+          price,
+          imageURL,
+          id,
+          // Specifications,
+        },
+      }
+    );
+  };
   const increaseHandler = () => {
     dispatch({ type: "INCREASE", price: price, id: id });
   };
@@ -15,29 +31,57 @@ const CartProduct = ({ title, price, imageURL, quantity, id }) => {
       setFade(0);
       setTimeout(() => dispatch({ type: "DELETE", price: price, id: id }), 200);
     } else {
-      dispatch({ type: "DECREASE", price:price,id: id });
+      dispatch({ type: "DECREASE", price: price, id: id });
     }
   };
+  const deleteHandler = () => {
+    setFade(0);
+    setTimeout(() => dispatch({ type: "DELETE", price: price, id: id }), 200);
+  };
+  const renderIcon = () => {
+    return quantity === 1 ? <FaTrash /> : <AiOutlineMinus />;
+  };
+
   return (
     <div style={{ scale: `${fade}` }} className={styles.productBlock}>
       <div>
-        <button className={styles.productBtn} onClick={increaseHandler}>
-          +
-        </button>
-        <p>{quantity}</p>
-        <button className={styles.productBtn} onClick={decreaseHandler}>
-          {quantity === 1 ? (
-            <FaTrash style={{ color: "#ef4444" }} />
-          ) : (
-            <AiOutlineMinus />
-          )}
-        </button>
-        <img className={styles.imageStyle} src={imageURL} alt={title} />
+        <div
+          style={{ scale: "1.3", marginBottom: "2.5rem", cursor: "pointer" }}
+          onClick={deleteHandler}
+        >
+          <AiOutlineClose />
+        </div>
+        <img
+          className={styles.imageStyle}
+          src={imageURL}
+          alt={title}
+          onClick={renderProductPage}
+        />
+        <section className={styles.productInfoBlock}>
+          <h4 onClick={renderProductPage}>{title}</h4>
+          <p>{price.toLocaleString("en")} تومان</p>
+          <div className={styles.productNumberController}>
+            <div>
+              <button onClick={increaseHandler}>+</button>
+              <p>{quantity}</p>
+              <button onClick={decreaseHandler}>{renderIcon()}</button>
+            </div>
+          </div>
+        </section>
       </div>
-      <div>
-        <h4>{title}</h4>
-        <h4>{price.toLocaleString("en")} تومان</h4>
-        <h4>تعداد : {quantity}</h4>
+      <div className={styles.productOptions}>
+        <div>
+          <FaSketch style={{ color: "#60a5fa" }} />
+          <p>ضمانت هفت روزه کالا</p>
+        </div>
+        <div>
+          <FaRibbon style={{ color: "#fbbf24" }} />
+          <p>18 ماه گارانتی</p>
+        </div>
+        <div>
+          <FaCheckCircle style={{ color: "#166534" }} />
+          <p>ارسال سریع</p>
+        </div>
       </div>
     </div>
   );

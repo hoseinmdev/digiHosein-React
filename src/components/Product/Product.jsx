474462fc83a2189a442bc97804bd39344de91071
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-02-06 20:44:43 +0330

    fix: optimization codes

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,43 +1,49 @@
-import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useCart } from "../../context/CartProvider";
 import styles from "./product.module.css";
 import { AiFillTag, AiOutlineShoppingCart } from "react-icons/ai";
-const Product = ({ title, price, imageURL, id, Specifications , comment}) => {
+
+const Product = ({ product }) => {
+  const { id, title, price, imageURL } = product;
   const { state, dispatch } = useCart();
-  const [product, setProduct] = useState({})
-  useEffect(() => setProduct({title, price, imageURL, id, Specifications, comment}), [])
   const history = useNavigate();
 
-
-  // const openInNewTab = (url) => {
-  //   const newWindow = window.open(url, "_blank", "noopener,noreferrer");
-  //   if (newWindow) newWindow.opener = null;
-  // };
-
-
-  const isInCart = state.cart.find((p) => p.id === id);
   const clickHandler = () => {
     toast.success("به سبد خرید اضافه شد");
     dispatch({
       type: "ADD_TO_CART",
-      product: {
-        id: id,
-        title: title,
-        price: price,
-        imageURL: imageURL,
-        quantity: 1,
-      },
+      product,
     });
   };
   const renderProductPage = () => {
     history(
       { pathname: `product/${id}` },
       {
-        state: product
+        state: product,
       }
     );
+  };
+  const renderAddToCartButton = () => {
+    const isInCart = state.cart.find((p) => p.id === id);
+    if (isInCart) {
+      return (
+        <Link to="/cart" className={`${styles.productButton} ${styles.inCart}`}>
+          <AiFillTag />
+          <p>در سبد خرید</p>
+        </Link>
+      );
+    } else {
+      return (
+        <button
+          className={`${styles.productButton} ${styles.addToCart}`}
+          onClick={clickHandler}
+        >
+          <AiOutlineShoppingCart style={{ transform: "scaleX(-1)" }} />
+          <p>افزودن به سبد خرید</p>
+        </button>
+      );
+    }
   };
   return (
     <div className={styles.productBlock}>
@@ -50,21 +56,7 @@ const Product = ({ title, price, imageURL, id, Specifications , comment}) => {
       <h4 className={styles.productTitle} onClick={renderProductPage}>
         {title}
       </h4>
-      <button disabled={isInCart} onClick={clickHandler}>
-        {isInCart ? (
-          <Link style={{ textDecoration: "none" }} to="cart">
-            <div className={styles.inCart}>
-              <AiFillTag />
-              <p>در سبد خرید</p>
-            </div>
-          </Link>
-        ) : (
-          <div className={styles.addToCart}>
-            <AiOutlineShoppingCart style={{ transform: "scaleX(-1)" }} />
-            <p>افزودن به سبد خرید</p>
-          </div>
-        )}
-      </button>
+      {renderAddToCartButton()}
       <p>{price.toLocaleString("en")} تومان</p>
     </div>
   );

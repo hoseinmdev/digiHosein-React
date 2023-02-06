5bb213a09d148051eee405883109d49142c72fe8
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-02-06 20:54:44 +0330

    fix: products context added

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ import HomePage from "./pages/HomePage/HomePage";
 import "react-toastify/dist/ReactToastify.css";
 import Toast from "./components/common/Toast/Toast";
 import SingleProductPage from "./pages/SingleProductPage/SingleProductPage";
+// import ProductProvider from "./context/ProductProvider";
+import ProductsProvider from "./context/ProductsProvider";
 function App() {
   return (
     <div className="App">
-        <Toast />
-      <CartProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="cart" element={<CartPage />} />
-          <Route path="product/:id" element={<SingleProductPage />} />
-        </Routes>
-      </CartProvider>
+      <Toast />
+      <ProductsProvider>
+        <CartProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="cart" element={<CartPage />} />
+            <Route path="product/:id" element={<SingleProductPage />} />
+          </Routes>
+        </CartProvider>
+      </ProductsProvider>
     </div>
   );
 }

ed13b8972ab530f1394cef61bf58e243ec082e9a
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-02-12 17:31:08 +0330

    feat: add slider and categories to home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,16 @@
-import BackToUpBtn from "../../components/common/Toast/BackToUpBtn";
-import Loading from "../../components/Loading/Loading";
+import CircleCategories from "../../components/CircleCategories/CircleCategories";
+import BackToUpBtn from "../../components/common/BackToUpBtn";
 import ProductList from "../../components/ProductList/ProductList";
+import Slider from "../../components/Slider/Slider";
 import Layout from "../../layout/Layout";
 
 const HomePage = () => {
   return (
     <>
       <Layout>
+        <Slider />
+        <CircleCategories/>
         <ProductList />
-        {/* <Loading /> */}
         <BackToUpBtn />
       </Layout>
     </>

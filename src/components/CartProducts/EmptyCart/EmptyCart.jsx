9f6ff84d9daa2af464b92c00a0f2a931c4c4a543
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-03-02 13:11:59 +0330

    feat: add component for cart when it is empty

---
+++ src/components/CartProducts/EmptyCart/EmptyCart.jsx
@@ -0,0 +1,20 @@
+import styles from "./EmptyCart.module.css";
+import emptyCart from "../../../Accets/images/empty-shopping-trolley.png";
+import { Link, NavLink } from "react-router-dom";
+const EmptyCart = () => {
+  return (
+    <div className={styles.emptyCartBlock}>
+      <img
+        style={{ width: "13rem", height: "13rem" }}
+        src={emptyCart}
+        alt="سبد شما خالی است"
+      ></img>
+      <h2 style={{ color: "#64748b" }}>سبد شما خالی است :(</h2>
+      <Link to="/">
+        <button className={styles.backToHomeBtn}>برو به صفحه اصلی</button>
+      </Link>
+    </div>
+  );
+};
+
+export default EmptyCart;

54b4264c2bdd1e1d31e3eb7893827c392080ed60
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-03-05 19:37:30 +0330

    add filter options

--- src/layout/CategorizedProduts/CategorizedProducts.jsx
+++ src/layout/CategorizedProduts/CategorizedProducts.jsx
@@ -1,4 +1,5 @@
 import styles from "./CategorizedProducts.module.css";
+import notFound from "../../Accets/images/notFoundProduct.png";
 import { ImArrowDown2, ImArrowUp2 } from "react-icons/im";
 import { useEffect, useState } from "react";
 import { RiFilterOffFill } from "react-icons/ri";
@@ -8,6 +9,7 @@ import { useParams, useSearchParams } from "react-router-dom";
 import SiteLayout from "layout/SiteLayout/SiteLayout";
 import Skeleton from "components/Skeleton/Skeleton";
 import createEmptyArray from "utils/createEmptyArray";
+import { toast } from "react-toastify";
 const CategorizedProducts = () => {
   const generateFiltersState = () => {
     const filters = {};
@@ -24,53 +26,67 @@ const CategorizedProducts = () => {
     }
     return allParams;
   };
+
   const { productState } = useProducts();
-  const [showFilters, setShowFilters] = useState(generateFiltersState);
   const currentCategory = useParams().category;
   const [category, setCategory] = useState();
+  const [showFilters, setShowFilters] = useState(generateFiltersState);
   const [products, setProducts] = useState();
-  const [producsForFilter, setProductsForFilter] = useState();
   const [searchParams, setSearchParams] = useSearchParams();
-  const allParams = serializeFormQuery();
 
   useEffect(() => {
-    const products = productState.allProducts.filter(
+    const allProducts = productState.allProducts.filter(
       (p) => p.category === currentCategory
     );
-    setProducts(0);
-    setProductsForFilter(0);
+    if (searchParams.toString().length === 0) {
+      setProducts(0);
+      setTimeout(() => setProducts(allProducts), 600);
+    }
     setCategory(0);
-    setTimeout(() => {
-      setCategory(currentCategory);
-      setProducts(products);
-      setProductsForFilter(products);
-    }, 1000);
+    setTimeout(() => setCategory(currentCategory), 600);
   }, [currentCategory]);
 
   useEffect(() => {
-    const allKeys = Object.keys(allParams);
-    const products = productState.allProducts.filter(
+    const allProducts = productState.allProducts.filter(
       (p) => p.category === currentCategory
     );
-    if (allKeys.length !== 0) {
-      allKeys.map((key) => {
+    const filteredProducts = Object.keys(serializeFormQuery())
+      .map((key) => {
         const finalKey = key.split("_");
-        const newProducts = producsForFilter.filter(
+        const filteredProducts = allProducts.filter(
           (p) => p[finalKey[0]] === parseInt(finalKey[1])
         );
-      setProductsForFilter(0);
-      setTimeout(() => setProductsForFilter(newProducts), 500);
-      });
+        return filteredProducts;
+      })
+      .flat(1);
+    if (searchParams.toString().length !== 0) {
+      setProducts(0);
+      setTimeout(() => setProducts(filteredProducts), 500);
     } else {
-      setProductsForFilter(0);
-      setTimeout(() => setProductsForFilter(products), 1000);
+      setProducts(0);
+      setTimeout(() => setProducts(allProducts), 600);
     }
   }, [searchParams]);
 
+  const renderDeleteFiltersBtn = () => {
+    return (
+      <button
+        onClick={() => setSearchParams({})}
+        className={
+          searchParams.toString().length === 0
+            ? styles.deleteFiltersBtn
+            : styles.ActiveDeleteFiltersBtn
+        }
+      >
+        حذف فیلتر ها <RiFilterOffFill />
+      </button>
+    );
+  };
   const toggleShowFilterOptionHandler = (key) => {
     setShowFilters({ ...showFilters, [key]: !showFilters[key] });
   };
   const checkBoxHandler = (e, optionKey) => {
+    const allParams = serializeFormQuery();
     if (e.target.checked) {
       setSearchParams({ ...allParams, [optionKey]: true });
     } else {
@@ -103,10 +119,14 @@ const CategorizedProducts = () => {
               style={{ display: showFilters[option.key] ? "flex" : "none" }}
             >
               {option.options.map((item) => {
+                const checked = Object.keys(serializeFormQuery()).find(
+                  (o) => o === item.key
+                );
                 return (
                   <label key={item.title}>
                     <input
                       type="checkbox"
+                      checked={checked ? true : false}
                       onChange={(e) => checkBoxHandler(e, item.key)}
                     />
                     {item.title}
@@ -130,24 +150,36 @@ const CategorizedProducts = () => {
       });
     }
   };
+  const renderNotFoundProduct = () => {
+    return (
+      <div className={styles.notFoundProductContainer}>
+        <img src={notFound} alt="محصولی یافت نشد" />
+        <h1>از این جستجو نتیجه ای نگرتیم ...</h1>
+      </div>
+    );
+  };
   const renderProducts = () => {
-    if (producsForFilter) {
-      return producsForFilter.map((p) => {
-        const product = {
-          id: p.id,
-          category: p.category,
-          title: p.title,
-          price: p.price,
-          imageURL: p.imageURL,
-          Specifications: p.Specifications,
-          comments: p.comments,
-          quantity: p.quantity,
-          technicalCheck: p.technicalCheck,
-          positivePoints: p.positivePoints,
-          negativePoints: p.negativePoints,
-        };
-        return <Product key={p.id} product={product} />;
-      });
+    if (products) {
+      if (products.length !== 0) {
+        return products.map((p, index) => {
+          const product = {
+            id: p.id,
+            category: p.category,
+            title: p.title,
+            price: p.price,
+            imageURL: p.imageURL,
+            Specifications: p.Specifications,
+            comments: p.comments,
+            quantity: p.quantity,
+            technicalCheck: p.technicalCheck,
+            positivePoints: p.positivePoints,
+            negativePoints: p.negativePoints,
+          };
+          return <Product key={index} product={product} />;
+        });
+      } else {
+        return renderNotFoundProduct();
+      }
     } else {
       return createEmptyArray(8).map((p, index) => {
         return (
@@ -161,19 +193,52 @@ const CategorizedProducts = () => {
       });
     }
   };
+  const renderProductSortBy = () => {
+    if (category) {
+      return (
+        <div className={styles.productsSortByContainer}>
+          <div>
+            <span>ترتیب :</span>
+          </div>
+          <div className={styles.porductsSortByOptions}>
+            <span>پرفروش ترین</span>
+            <span>کمترین قیمت</span>
+            <span>بیشترین قیمت</span>
+            <span>جدیدترین</span>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.porductsSortByOptions} style={{gap:"1.5rem"}}>
+          {createEmptyArray(5).map((p, index) => {
+            return (
+              <Skeleton
+                key={index}
+                width={"5rem"}
+                height={"1.8rem"}
+                radius={"8px"}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+  };
   return (
     <SiteLayout>
       <div className={styles.allContentContainer}>
         <div className={styles.filterOptionsContainer}>
           <div className={styles.filterOptionsTitle}>
             <h3>فیلتر ها</h3>
-            <button>
-              حذف فیلتر ها <RiFilterOffFill />
-            </button>
+            {renderDeleteFiltersBtn()}
           </div>
           {renderFilters()}
         </div>
-        <div className={styles.productsContainer}>{renderProducts()}</div>
+        <div className={styles.productsContainer}>
+          {renderProductSortBy()}
+          <div className={styles.products}>{renderProducts()}</div>
+        </div>
       </div>
     </SiteLayout>
   );

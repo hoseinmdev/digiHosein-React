17724a8c82e9834eef6af2bb7cac68e5983b5014
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-03-06 19:23:41 +0330

    seprate cart button part

--- src/components/Navigation/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation/Navigation.jsx
@@ -1,48 +1,48 @@
 import styles from "./navigation.module.css";
 import digiHosein from "../../../Accets/images/logo.jpg";
 import {
-  FaShoppingCart,
   FaHome,
   FaHeadphonesAlt,
   FaLaptopCode,
   FaTabletAlt,
 } from "react-icons/fa";
 import { BiGame } from "react-icons/bi";
-import { Link, NavLink } from "react-router-dom";
-import { useCart } from "../../../context/CartProvider";
+import { NavLink } from "react-router-dom";
 import { GoDeviceMobile } from "react-icons/go";
 import { BsSmartwatch, BsSpeakerFill } from "react-icons/bs";
 import SearchProducts from "../SearchProducts/SearchProducts";
+import CartButton from "../CartButton/CartButton";
+
+const items = [
+  { title: "خانه", path: "/", icon: <FaHome /> },
+  { title: "موبایل", path: "/categories/phones", icon: <GoDeviceMobile /> },
+  { title: "تبلت", path: "/categories/tablets", icon: <FaTabletAlt /> },
+  { title: "لپتاپ", path: "/categories/laptops", icon: <FaLaptopCode /> },
+  {
+    title: "اسپیکر و بلندگو",
+    path: "/categories/speakers",
+    icon: <BsSpeakerFill />,
+  },
+  {
+    title: "ساعت هوشمند",
+    path: "/categories/digitalWatches",
+    icon: <BsSmartwatch />,
+  },
+  {
+    title: "هدفون و هندزفری",
+    path: "/categories/headphones",
+    icon: <FaHeadphonesAlt />,
+  },
+  { title: "گیمینگ", path: "/categories/consoles", icon: <BiGame /> },
+];
+
 const Navigation = () => {
-  const { state } = useCart();
   const navLinkStyle = (isActive) => {
     return isActive
       ? `${styles.isActive} ${styles.optionContainer}`
       : `${styles.isNotActive} ${styles.optionContainer}`;
   };
 
-  const items = [
-    { title: "خانه", path: "/", icon: <FaHome /> },
-    { title: "موبایل", path: "/categories/phones", icon: <GoDeviceMobile /> },
-    { title: "تبلت", path: "/categories/tablets", icon: <FaTabletAlt /> },
-    { title: "لپتاپ", path: "/categories/laptops", icon: <FaLaptopCode /> },
-    {
-      title: "اسپیکر و بلندگو",
-      path: "/categories/speakers",
-      icon: <BsSpeakerFill />,
-    },
-    {
-      title: "ساعت هوشمند",
-      path: "/categories/digitalWatches",
-      icon: <BsSmartwatch />,
-    },
-    {
-      title: "هدفون و هندزفری",
-      path: "/categories/headphones",
-      icon: <FaHeadphonesAlt />,
-    },
-    { title: "گیمینگ", path: "/categories/consoles", icon: <BiGame /> },
-  ];
   return (
     <div className={styles.siteNvigation}>
       <div className={styles.topOfNavigation}>
@@ -52,14 +52,7 @@ const Navigation = () => {
         </div>
         <div>
           <button className={styles.submitButton}>ورود | ثبت نام</button>
-          <Link to="/cart">
-            <button className={styles.cartButton}>
-              <FaShoppingCart />
-              <span className={styles.productsInCartCounter}>
-                {state.cart.length}
-              </span>
-            </button>
-          </Link>
+          <CartButton />
         </div>
       </div>
       <div className={styles.downOfNavigation}>

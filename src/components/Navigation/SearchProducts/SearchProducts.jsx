32f8130c7170294075d7aaf441771a40d726ffed
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-03-11 23:19:06 +0330

    add search product Property

--- src/components/Navigation/SearchProducts/SearchProducts.jsx
+++ src/components/Navigation/SearchProducts/SearchProducts.jsx
@@ -1,11 +1,76 @@
+import Product from "components/Product/Product";
+import { useProducts } from "context/ProductsProvider";
+import { useState } from "react";
+import { BsFillCartCheckFill } from "react-icons/bs";
+import { HiSquares2X2 } from "react-icons/hi2";
 import { FaSearch } from "react-icons/fa";
+import FoundProduct from "../FoundProduct/FoundProduct";
 import styles from "./SearchProducts.module.css";
 const SearchProducts = () => {
+  const [searchBoxSelected, setSearchBoxSelected] = useState(0);
+  const [searchInput, setSearchInput] = useState("");
+  const [productsFound, setProductsFound] = useState("");
+  const { productState } = useProducts();
+
+  const searchBoxSelectedHandler = () => {
+    setSearchBoxSelected(!searchBoxSelected);
+    setSearchInput("");
+    setProductsFound("");
+  };
+  const searchInputHandler = (e) => {
+    setSearchInput(e.target.value);
+    const findedProducts = productState.allProducts.filter((p) =>
+      p.title.includes(e.target.value)
+    );
+    setProductsFound(findedProducts);
+  };
+  const renderBackdrop = () => {
+    return (
+      <div className={styles.backdrop} onClick={searchBoxSelectedHandler}></div>
+    );
+  };
   return (
-    <div className={styles.searchBox}>
-      <FaSearch />
-      <input placeholder="جستجو برای ... " />
-    </div>
+    <>
+      <div
+        className={
+          !searchBoxSelected ? styles.searchBox : styles.activeSearchBox
+        }
+        onClick={() => (!searchBoxSelected ? searchBoxSelectedHandler() : "")}
+      >
+        <FaSearch />
+        <input
+          placeholder="محصول یا برند مورد نظرتان را جستجو کنید ... "
+          value={searchInput}
+          onChange={(e) => searchInputHandler(e)}
+        />
+      </div>
+      {productsFound && (
+        <div className={styles.productsContainer}>
+          <span>
+            <HiSquares2X2 />
+            جستجو برای ...
+            <p>{searchInput}</p>
+          </span>
+          {productsFound.length !== productState.allProducts.length ? (
+            <div className={styles.products}>
+              {productsFound.map((p) => {
+                return (
+                  <FoundProduct
+                    key={p.id}
+                    id={p.id}
+                    imageURL={p.imageURL}
+                    title={p.title}
+                  />
+                );
+              })}
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+      )}
+      {searchBoxSelected ? renderBackdrop() : ""}
+    </>
   );
 };
 

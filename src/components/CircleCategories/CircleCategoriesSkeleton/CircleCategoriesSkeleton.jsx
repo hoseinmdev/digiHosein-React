3e47edb01b65b9e168fe57a3f11bd65555285ce2
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-03-31 20:06:36 +0430

    css for mobile added

--- src/components/CircleCategories/CircleCategoriesSkeleton/CircleCategoriesSkeleton.jsx
+++ src/components/CircleCategories/CircleCategoriesSkeleton/CircleCategoriesSkeleton.jsx
@@ -7,8 +7,21 @@ const skeletons = createEmptyArray(6).map((_, i) => (
     <Skeleton width={"6rem"} height={"2rem"} radius={"15px"} />
   </div>
 ));
+const mobileSkeletons = createEmptyArray(3).map((_, i) => (
+  <div key={i}>
+    <Skeleton width={"6.5rem"} height={"6.5rem"} radius={"100%"} />
+    <Skeleton width={"5rem"} height={"1.5rem"} radius={"15px"} />
+  </div>
+));
 const CircleCategoriesSkeleton = () => {
-  return <div className={styles.categoriesSkeletonContainer}>{skeletons}</div>;
+  return (
+    <>
+      <div className={styles.categoriesSkeletonContainer}>{skeletons}</div>
+      <div className={styles.categoriesSkeletonContainerMobile}>
+        {mobileSkeletons}
+      </div>
+    </>
+  );
 };
 
 export default CircleCategoriesSkeleton;

25a8b25fa80da832d66c6370aeab5fd9b18e95ad
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-04-22 14:40:02 +0330

    code updated

--- src/components/SingleProduct/ProductIntroduction/ProductIntroduction.jsx
+++ src/components/SingleProduct/ProductIntroduction/ProductIntroduction.jsx
@@ -1,114 +1,24 @@
-import { BsCpu } from "react-icons/bs";
-import { FaDigitalTachograph } from "react-icons/fa";
-import { FiCamera } from "react-icons/fi";
-import { GiBatteryPackAlt, GiWeight } from "react-icons/gi";
-import { MdScreenshot, MdStorage } from "react-icons/md";
-import { CgSmartphoneRam } from "react-icons/cg";
 import styles from "./productIntroduction.module.css";
 const ProductIntroduction = ({ product }) => {
-    const productProperties = []
-  Object.values(product.Specifications).forEach(function(value, index) {
-    productProperties.push({title: value.title, icon: value.icon})
+  const productProperties = [];
+  Object.values(product.Specifications).forEach(function (value, index) {
+    productProperties.push({ title: value.title, icon: value.icon });
   });
-  console.log(productProperties)
-  const renderProductProperties = () => {
-    if (product.category === "phones") {
-      return (
-        <div className={styles.infoList}>
-
-          {productProperties.map(p => {
-            return <div key={p.title}>
-                          {p.icon}
-            <p>{p.title}</p>
-            </div>
-          })}
-
-          {/* <div>
-            <BsCpu className={styles.iconBlock} />
-            <p>{product.Specifications.cpu.model}</p>
-          </div>
-          <div>
-            <FaDigitalTachograph className={styles.iconBlock} />
-            <p>{product.Specifications.storage.capacity}</p>
-          </div>
-          <div>
-            <MdScreenshot className={styles.iconBlock} />
-            <p>{product.Specifications.screen.size}</p>
-          </div>
-          <div>
-            <FiCamera className={styles.iconBlock} />
-            <p>{product.Specifications.camera.quality}</p>
-          </div>
-          <div>
-            <GiBatteryPackAlt className={styles.iconBlock} />
-            <p>{product.Specifications.battery.capacity}</p>
-          </div>
-          <div>
-            <CgSmartphoneRam className={styles.iconBlock} />
-            <p>{product.Specifications.ram.capacity}</p>
-          </div> */}
-        </div>
-      );
-    }
-    if (product.category === "laptops") {
-      return (
-        <div className={styles.infoList}>
-          <div>
-            <BsCpu className={styles.iconBlock} />
-            <p>{product.Specifications.cpu.model}</p>
-          </div>
-          <div>
-            <FaDigitalTachograph className={styles.iconBlock} />
-            <p>{product.Specifications.gpu.model}</p>
-          </div>
-          <div>
-            <MdStorage className={styles.iconBlock} />
-            <p>{product.Specifications.storage.capacity}</p>
-          </div>
-          <div>
-            <GiWeight className={styles.iconBlock} />
-            <p>{product.Specifications.Weight.capacity}</p>
-          </div>
-          <div>
-            <CgSmartphoneRam className={styles.iconBlock} />
-            <p>{product.Specifications.ram.capacity}</p>
-          </div>
-        </div>
-      );
-    }
-    if (product.type === "consoles") {
-      return (
-        <div className={styles.infoList}>
-          <div>
-            <BsCpu className={styles.iconBlock} />
-            <p>{product.Specifications.cpu}</p>
-          </div>
-          <div>
-            <CgSmartphoneRam className={styles.iconBlock} />
-            <p>{product.Specifications.ram}</p>
-          </div>
-          <div>
-            <FiCamera className={styles.iconBlock} />
-            <p>{product.Specifications.camera}</p>
-          </div>
-          <div>
-            <MdStorage className={styles.iconBlock} />
-            <p>{product.Specifications.storage}</p>
-          </div>
-          <div>
-            <GiWeight className={styles.iconBlock} />
-            <p>{product.Specifications.Weight}</p>
-          </div>
-        </div>
-      );
-    }
-  };
   return (
     <>
       <div className={styles.imageAndInfoBlock}>
         <div className={styles.infoBlock}>
           <h3>ویژگی های اصلی</h3>
-          {renderProductProperties()}
+          <div className={styles.infoList}>
+            {productProperties.map((p) => {
+              return (
+                <div key={p.title}>
+                  {p.icon}
+                  <p>{p.title}</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
         <img
           className={styles.imageStyle}
@@ -124,7 +34,16 @@ const ProductIntroduction = ({ product }) => {
         ></img>
         <div className={styles.infoBlock}>
           <h3>ویژگی های اصلی</h3>
-          {renderProductProperties()}
+          <div className={styles.infoList}>
+            {productProperties.map((p) => {
+              return (
+                <div key={p.title}>
+                  {p.icon}
+                  <p>{p.title}</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </>

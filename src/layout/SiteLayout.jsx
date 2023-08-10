84c6b677ca6ef839c26c9b1f8403a29c33735750
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-08-10 13:36:21 +0330

    feat: add animation

--- src/layout/SiteLayout.jsx
+++ src/layout/SiteLayout.jsx
@@ -1,27 +1,12 @@
-import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation/Navigation";
-import backToUp from "utils/BackToUp";
 const SiteLayout = ({ children }) => {
-  const [fade, setFade] = useState(0);
-  useEffect(() => {
-    backToUp();
-    setTimeout(() => {
-      setFade(1);
-    }, 100);
-  }, []);
-
   return (
-    <>
-      <div
-        className="flex w-full max-w-[1360px] flex-col items-center justify-center gap-2 text-black lg:gap-6"
-        style={{ opacity: fade }}
-      >
-        <Navigation />
-        {children}
-        <Footer />
-      </div>
-    </>
+    <div className="flex w-full max-w-[1360px] flex-col items-center justify-center gap-2 text-black lg:gap-6">
+      <Navigation />
+      <div className="fadeShow w-full">{children}</div>
+      <Footer />
+    </div>
   );
 };
 

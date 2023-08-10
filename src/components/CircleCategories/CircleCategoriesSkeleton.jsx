c85972a13ec7f1095505c571ca2728b304f0b2b7
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-08-10 13:38:08 +0330

    fix: style bug

--- src/components/CircleCategories/CircleCategoriesSkeleton.jsx
+++ src/components/CircleCategories/CircleCategoriesSkeleton.jsx
@@ -2,13 +2,13 @@ import createEmptyArray from "utils/createEmptyArray";
 import Skeleton from "../Skeleton";
 
 const skeletons = createEmptyArray(6).map((_, i) => (
-  <div key={i} className="flex flex-col items-center justify-center gap-4">
+  <div key={i} className="p-4 flex flex-col items-center justify-center gap-4">
     <Skeleton width={"8.5rem"} height={"8.5rem"} radius={"100%"} />
     <Skeleton width={"6rem"} height={"2rem"} radius={"15px"} />
   </div>
 ));
 const mobileSkeletons = createEmptyArray(3).map((_, i) => (
-  <div key={i} className="flex flex-col items-center justify-center gap-4">
+  <div key={i} className=" py-4 flex flex-col items-center justify-center gap-4">
     <Skeleton width={"6.5rem"} height={"6.5rem"} radius={"100%"} />
     <Skeleton width={"5rem"} height={"1.5rem"} radius={"15px"} />
   </div>

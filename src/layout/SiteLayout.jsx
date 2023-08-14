1b7cbacbf47c4c832ac6f596b7da22a5b644962c
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-08-14 11:55:19 +0330

    feat: add animation

--- src/layout/SiteLayout.jsx
+++ src/layout/SiteLayout.jsx
@@ -1,9 +1,9 @@
+import Header from "components/Header";
 import Footer from "../components/Footer";
-import Navigation from "../components/Navigation/Navigation";
 const SiteLayout = ({ children }) => {
   return (
     <div className="flex w-full max-w-[1360px] flex-col items-center justify-center gap-2 text-black lg:gap-6">
-      <Navigation />
+      <Header />
       <div className="fadeShow w-full">{children}</div>
       <Footer />
     </div>

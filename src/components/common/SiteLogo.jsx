e77e87cccdcfedb7901f17358688deb76c4cc4ff
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-08-14 11:53:25 +0330

    feat: implement some component

---
+++ src/components/common/SiteLogo.jsx
@@ -0,0 +1,9 @@
+import logo from "../../assets/images/logo.jpg";
+
+const SiteLogo = () => {
+  return (
+    <img src={logo} alt="دیجی حسین" className="h-16 w-28  lg:h-20 lg:w-[12rem]" />
+  );
+};
+
+export default SiteLogo;

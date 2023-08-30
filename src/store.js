31241fd71d5aaaab398790b9811940b68a991ae5
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-08-30 19:48:21 +0330

    feat: add localStorage middleware to store configure

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "redux/cartSlice"
+import cartReducer, { localStorageMiddleware } from "redux/cartSlice";
 export const store = configureStore({
   reducer: { cart: cartReducer },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
 });

19668070e089d1f40bdc518560025cd185a63cb0
“HoseinMahmoudi” <Hoseindev@gmail.com>
2023-09-04 16:00:06 +0330

    add space between components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,9 +7,10 @@ import ProductsSlider from "components/HomePage/ProductsSlider";
 const HomePage = () => {
   return (
     <SiteLayout>
-      <Slider />
-      <CircleCategories />
-      {/* <ProductsSlider
+      <div className="flex flex-col gap-4">
+        <Slider />
+        <CircleCategories />
+        {/* <ProductsSlider
         bgColor="bg-gray-900"
         title="پرفروش ترین محصولات"
         titleColor="#f8fafc"
@@ -19,18 +20,23 @@ const HomePage = () => {
           return <Product key={product.id} product={product} />;
         })}
       </ProductsSlider> */}
-      <ProductsSlider
-        title="موبایل ها"
-        bgColor="bg-indigo-100"
-        category="phones"
-      />
-      <LandingBanner />
-      <ProductsSlider
-        title="لپتاپ ها"
-        bgColor="bg-violet-100"
-        category="laptops"
-      />
-      <ProductsSlider title="تبلت ها" bgColor="bg-rose-50" category="tablets" />
+        <ProductsSlider
+          title="موبایل ها"
+          bgColor="bg-indigo-100"
+          category="phones"
+        />
+        <LandingBanner />
+        <ProductsSlider
+          title="لپتاپ ها"
+          bgColor="bg-violet-100"
+          category="laptops"
+        />
+        <ProductsSlider
+          title="تبلت ها"
+          bgColor="bg-rose-50"
+          category="tablets"
+        />
+      </div>
       <BackToUpBtn />
     </SiteLayout>
   );
